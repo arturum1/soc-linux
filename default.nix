d9bb0d2921db3571ca43cdc63f867a95f1c512bb
Artur Nóbrega <arturnobrega1999@gmail.com>
2026-01-15 16:17:42 +0000

    feat(py2hwsw): Update py2hwsw version
    New version includes pre-built auto-generated tests for Linux kernel modules of iob_timer peripheral.

--- default.nix
+++ default.nix
@@ -5,8 +5,8 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/25.05.tar.gz") {} }:
 
 let
-  py2hwsw_commit = "63d81d8a0b6cd97755a652617bb71e4f644ad3eb"; # Replace with the desired commit.
-  py2hwsw_sha256 = "r5UAe5px50z3wXb56Vxn8/kAS/gE5b0MZcU1UTA8Hmc="; # Replace with the actual SHA256 hash.
+  py2hwsw_commit = "4ce6c2ab15d270b3a0e58c4f2d3cab5c40cfe85a"; # Replace with the desired commit.
+  py2hwsw_sha256 = "Wq9DEjw3Df0PoraZm5v1TL45nNqYueAUdXXeAVkYMy4="; # Replace with the actual SHA256 hash.
   # Get local py2hwsw root from `PY2HWSW_ROOT` env variable
   py2hwswRoot = builtins.getEnv "PY2HWSW_ROOT";
 
@@ -24,13 +24,18 @@ let
             # Root provided, use local
             pkgs.lib.cleanSource py2hwswRoot
           else
-            # No root provided, use GitHub
+            # No root provided: fetch from GitHub and add shortHash.tex
             (pkgs.fetchFromGitHub {
               owner = "IObundle";
               repo = "py2hwsw";
               rev = py2hwsw_commit;
               sha256 = py2hwsw_sha256;
               fetchSubmodules = true;
+	      # Generate shortHash.tex based on commit
+              postFetch = ''
+                echo "Creating shortHash.tex"
+                echo "${builtins.substring 0 7 py2hwsw_commit}" > "$out/py2hwsw/shortHash.tex"
+              '';
             }).overrideAttrs (_: {
               GIT_CONFIG_COUNT = 1;
               GIT_CONFIG_KEY_0 = "url.https://github.com/.insteadOf";
